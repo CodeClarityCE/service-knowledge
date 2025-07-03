c9162530133ef778055aa66f86da258c9894365d
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-07-03 11:24:45 +0200

    Refactor UpdateLicenses function to use transaction for database operations

--- src/utilities/pgsql/licenses.go
+++ src/utilities/pgsql/licenses.go
@@ -2,29 +2,44 @@ package pgsql
 
 import (
 	"context"
+	"fmt"
 
 	knowledge "github.com/CodeClarityCE/utility-types/knowledge_db"
 	"github.com/uptrace/bun"
 )
 
 func UpdateLicenses(db *bun.DB, licenses []knowledge.License) error {
+	// Start a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
 
 	for _, license := range licenses {
-
-		exists, err := db.NewSelect().Model(&license).Where("\"licenseId\" = ?", license.LicenseID).Exists(context.Background())
+		exists, err := tx.NewSelect().Model(&license).Where("\"licenseId\" = ?", license.LicenseID).Exists(context.Background())
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to check existence for license %v: %w", license.LicenseID, err)
 		}
 
 		if exists {
-			_, err = db.NewUpdate().Model(&license).Where("\"licenseId\" = ?", license.LicenseID).Exec(context.Background())
+			_, err = tx.NewUpdate().Model(&license).Where("\"licenseId\" = ?", license.LicenseID).Exec(context.Background())
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to update license %v: %w", license.LicenseID, err)
 			}
 		} else {
-			_, err = db.NewInsert().Model(&license).Exec(context.Background())
+			_, err = tx.NewInsert().Model(&license).Exec(context.Background())
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to insert license %v: %w", license.LicenseID, err)
 			}
 		}
 	}
