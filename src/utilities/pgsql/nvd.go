ccbccae673fb17c4fee16e0ed34639453851fcc2
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-07-03 11:30:41 +0200

    Refactor UpdateNvd function to use transactions and improve error handling

--- src/utilities/pgsql/nvd.go
+++ src/utilities/pgsql/nvd.go
@@ -2,30 +2,48 @@ package pgsql
 
 import (
 	"context"
-	"log"
+	"fmt"
 
 	knowledge "github.com/CodeClarityCE/utility-types/knowledge_db"
 	"github.com/uptrace/bun"
 )
 
-func UpdateNvd(db *bun.DB, nvd []knowledge.NVDItem) {
+func UpdateNvd(db *bun.DB, nvd []knowledge.NVDItem) error {
+	// Start a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+
+	ctx := context.Background()
 	for _, vuln := range nvd {
-		ctx := context.Background()
-		exists, err := db.NewSelect().Model(&vuln).Where("nvd_id = ?", vuln.NVDId).Exists(ctx)
+		exists, err := tx.NewSelect().Model(&vuln).Where("nvd_id = ?", vuln.NVDId).Exists(ctx)
 		if err != nil {
-			log.Println(err)
+			return fmt.Errorf("failed to check existence for NVD item %v: %w", vuln.NVDId, err)
 		}
 
 		if !exists {
-			_, err = db.NewInsert().Model(&vuln).Exec(context.Background())
+			_, err = tx.NewInsert().Model(&vuln).Exec(ctx)
 			if err != nil {
-				log.Println(err)
+				return fmt.Errorf("failed to insert NVD item %v: %w", vuln.NVDId, err)
+			}
+		} else {
+			_, err = tx.NewUpdate().Model(&vuln).WherePK().Exec(ctx)
+			if err != nil {
+				return fmt.Errorf("failed to update NVD item %v: %w", vuln.NVDId, err)
 			}
 		}
-
-		_, err = db.NewUpdate().Model(&vuln).WherePK().Exec(context.Background())
-		if err != nil {
-			log.Println(err)
-		}
 	}
+
+	return nil
 }
