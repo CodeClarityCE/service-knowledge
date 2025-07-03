9eb35bb2c996f6e7dbfd68bc9f9a90b349d53e5d
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-07-03 12:36:22 +0200

    Add integration test for Update function in nvd package

---
+++ src/mirrors/nvd/main_test.go
@@ -0,0 +1,35 @@
+package nvd
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+	"time"
+
+	dbhelper "github.com/CodeClarityCE/utility-dbhelper/helper"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+)
+
+func TestUpdate(t *testing.T) {
+	os.Setenv("NVD_API_KEY", "")
+	os.Setenv("PG_DB_HOST", "127.0.0.1")
+	os.Setenv("PG_DB_PORT", "5432")
+	os.Setenv("PG_DB_USER", "postgres")
+	os.Setenv("PG_DB_PASSWORD", "!ChangeMe!")
+	dsn_knowledge := "postgres://postgres:!ChangeMe!@127.0.0.1:5432/" + dbhelper.Config.Database.Knowledge + "?sslmode=disable"
+	sqldb_knowledge := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn_knowledge), pgdriver.WithTimeout(50*time.Second)))
+	db_knowledge := bun.NewDB(sqldb_knowledge, pgdialect.New())
+	defer db_knowledge.Close()
+
+	dsn_config := "postgres://postgres:!ChangeMe!@127.0.0.1:5432/" + dbhelper.Config.Database.Config + "?sslmode=disable"
+	sqldb_config := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn_config), pgdriver.WithTimeout(50*time.Second)))
+	db_config := bun.NewDB(sqldb_config, pgdialect.New())
+	defer db_knowledge.Close()
+
+	err := Update(db_knowledge, db_config)
+	if err != nil {
+		t.Fatalf("Update failed: %v", err)
+	}
+}
